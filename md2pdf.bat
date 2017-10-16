3ddec6ba263fc30a89291606e949ec448505fad3
James Edington <jedingto@peraton.com>
2017-10-16 14:28:58 -0500

    PDFs go into a PDF folder
    Skeleton code for Unix support added

--- md2pdf.bat
+++ md2pdf.bat
@@ -1,10 +1,18 @@
+rem(){ :;};rem '
+@echo off
+@goto b
+';echo SHELL SUPPORT TODO;exit 1
+:b
+@echo on
 set INPUT=%1
-set TEMPLATE=mla.tex
-set PDF=%~n1.pdf
-set TEMP=TeMp_
+set TEMPLATE=%~dp0\mla.tex
+set PDFDIR=%~dp1\PDFs
+set PDF=%PDFDIR%\%~n1.pdf
+set TEMP=%PDFDIR%\_TeMp
 
+if not exist "%PDFDIR%" mkdir "%PDFDIR%"
 pandoc --verbose -s -S %INPUT% --from=markdown --template "%TEMPLATE%" -o "%TEMP%.tex"
-pdflatex -interaction=nonstopmode "%TEMP%.tex"
+pdflatex -interaction=nonstopmode "%TEMP%.tex" -output-directory="%PDFDIR%"
 move "%TEMP%.pdf" "%PDF%"
 del "%TEMP%.*"
 start "" "%PDF%"
